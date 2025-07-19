dfb2f2d9831ac2c7f53a4d15924adc7a62a7c349
yuet25chan <chanyuet1993@gmail.com>
2025-07-19 13:58:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+
 console.log("Script Connected");
+
+const message = document.getElementById("message");
+
+function greeting() {
+  if (message.innerText === "Good Morning") {
+    message.innerText = "Good Evening";
+  } else {
+    message.innerText = "Good Morning";
+  }
+}
+message.addEventListener(onclick, greeting);
